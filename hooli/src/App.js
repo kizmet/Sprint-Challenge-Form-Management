7222e522e39f7a6eb8de41a9462267c3ae56a9de
Bryant Patton <bryantpatton@gmail.com>
2019-08-09 14:11:39 -0700

    Recipes

--- hooli/src/App.js
+++ hooli/src/App.js
@@ -1,12 +1,50 @@
 import React from "react";
 import Login from "./components/forms/Login";
+import Register from "./components/forms/Register";
+import Recipes from "./components/Recipes";
 import "./App.css";
+import { Layout, Menu } from "antd";
+import { useRoutes, A } from "hookrouter";
+import Home from "./Home";
+import "antd/dist/antd.css";
+
+const routes = {
+  "/": () => <Home />,
+  "/register": () => <Register />,
+  "/login": () => <Login />,
+  "/recipes": () => <Recipes />
+};
 
 function App() {
+  const routeResult = useRoutes(routes);
+  const { Header, Footer, Sider, Content } = Layout;
   return (
-    <div className="App">
-      <header className="App-header">Learn React!</header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          style={{ lineHeight: "64px" }}
+        >
+          <Menu.Item key="1">
+            <A href="/">Home Page</A>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <A href="/login">Login Page</A>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <A href="/register">Register Page</A>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <A href="/recipes">Recipies Page</A>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: "0 50px" }}>{routeResult}</Content>
+      <Footer style={{ textAlign: "center" }}>Created by Bryant Patton</Footer>
+    </Layout>
   );
 }
 
--- hooli/src/components/forms/Login.js
+++ hooli/src/components/forms/Login.js
@@ -1,189 +1,165 @@
-import React from "react";
-import * as Yup from "yup";
-import { Formik } from "formik";
-import {
-  Row,
-  Col,
-  Layout,
-  Card,
-  Icon,
-  Form,
-  Input,
-  Button,
-  Checkbox,
-  Alert,
-  notification
-} from "antd";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Field, withFormik, Formik } from "formik";
+import * as Yup from "yup";
+import {
+  Form,
+  List,
+  Input,
+  Icon,
+  Radio,
+  Select,
+  Checkbox,
+  Button,
+  Card,
+  Alert
+} from "antd";
 
 const url = "http://localhost:5000/api/login";
 
-const LoginSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Please use a valid email")
-    .required("Required"),
-  password: Yup.string().required("Required"),
-  last_name: Yup.string().required("Required"),
-  company: Yup.string().required("Required")
-});
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 }
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 }
+  }
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0
+    },
+    sm: {
+      span: 16,
+      offset: 8
+    }
+  }
+};
+const LoginForm = ({
+  errors,
+  touched,
+  values,
+  handleSubmit,
+  handleChange,
+  handleBlur,
+  status,
+  setFieldValue,
+  setFieldTouched,
+  name
+}) => {
+  const [user, setUser] = useState([]);
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      errors: null
+  useEffect(() => {
+    if (status) {
+      setUser([...user, status]);
+    }
+  }, [status]);
+
+  return (
+    <Card
+      title="Enter Login Credentials"
+      style={{
+        maxWidth: 400,
+        minWidth: 400,
+        marginLeft: "auto",
+        marginRight: "auto"
+      }}
+    >
+      {errors.name &&
+        touched.name &&
+        errors.map(error => {
+          return (
+            <Alert
+              description={error.name}
+              type="error"
+              style={{ marginBottom: 2 }}
+              closable
+            />
+          );
+        })}
+
+      <Form {...formItemLayout} onSubmit={handleSubmit}>
+        <Form.Item
+          label="Username"
+          {...(errors.username && touched.username
+            ? {
+                validateStatus: "error",
+                help: errors.username
+              }
+            : {})}
+        >
+          <Input
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.username}
+            placeholder="username"
+            label="username"
+            name="username"
+            id="username"
+            type="username"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          {...(errors.password && touched.password
+            ? {
+                validateStatus: "error",
+                help: errors.password
+              }
+            : {})}
+        >
+          <Input
+            prefix={<Icon type="bank" style={{ color: "rgba(0,0,0,.25)" }} />}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+            placeholder="password"
+            label="password"
+            name="password"
+            id="password"
+            type="password"
+          />
+        </Form.Item>
+
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
+
+const Login = withFormik({
+  mapPropsToValues({ password, username }) {
+    return {
+      password: password || "password",
+      username: username || "test"
     };
+  },
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required("Required"),
+    password: Yup.string().required("Required")
+  }),
+
+  handleSubmit(values, actions) {
+    axios
+      .post(url, values)
+      .then(res => {
+        actions.setStatus(res.data);
+        console.log(res);
+      })
+      .catch(err => {
+        // actions.setErrors(err.message);
+        console.log(err.message);
+      });
   }
-
-  render() {
-    return (
-      <Card
-        title="New User Sign Up"
-        style={{
-          maxWidth: 400,
-          minWidth: 400,
-          marginLeft: "auto",
-          marginRight: "auto"
-        }}
-      >
-        {this.state.errors &&
-          this.state.errors.map(error => {
-            return (
-              <Alert
-                description={error}
-                type="error"
-                style={{ marginBottom: 10 }}
-                closable
-              />
-            );
-          })}
-
-        <Formik
-          initialValues={{
-            first_name: "",
-            last_name: "",
-            email: "",
-            company: ""
-          }}
-          validationSchema={LoginSchema}
-          onSubmit={(values, actions) => {
-            let formData = new FormData();
-            formData.append("first_name", values.first_name);
-            formData.append("last_name", values.last_name);
-            formData.append("email", values.last_name);
-            formData.append("company", values.last_name);
-            axios
-              .post(url, formData, requestConfig)
-              .then(response => {
-                if (response.data.success) {
-                  notification["success"]({
-                    message: "Success",
-                    description: "Thanks for singing up!"
-                  });
-                } else {
-                  this.setState({
-                    errors: response.data.errors
-                  });
-                }
-
-                actions.setSubmitting(false);
-              })
-              .catch(function(response) {
-                actions.setSubmitting(false);
-                console.log(response);
-              });
-          }}
-          render={props => (
-            <Form className="signup-form" onSubmit={props.handleSubmit}>
-              <Form.Item
-                {...(props.errors.first_name && props.touched.first_name
-                  ? {
-                      validateStatus: "error",
-                      help: props.errors.first_name
-                    }
-                  : {})}
-              >
-                <Input
-                  prefix={
-                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  placeholder="First name"
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.first_name}
-                  name="first_name"
-                />
-              </Form.Item>
-
-              <Form.Item
-                {...(props.errors.last_name && props.touched.last_name
-                  ? {
-                      validateStatus: "error",
-                      help: props.errors.last_name
-                    }
-                  : {})}
-              >
-                <Input
-                  prefix={
-                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  placeholder="Last name"
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.last_name}
-                  name="last_name"
-                />
-              </Form.Item>
-
-              <Form.Item
-                {...(props.errors.email && props.touched.email
-                  ? { validateStatus: "error", help: props.errors.email }
-                  : {})}
-              >
-                <Input
-                  prefix={
-                    <Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  placeholder="Email"
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.email}
-                  name="email"
-                />
-              </Form.Item>
-
-              <Form.Item
-                {...(props.errors.company && props.touched.company
-                  ? { validateStatus: "error", help: props.errors.username }
-                  : {})}
-              >
-                <Input
-                  prefix={
-                    <Icon type="bank" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  placeholder="Comapny"
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.company}
-                  name="company"
-                />
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  className="signup-form-button"
-                  loading={props.isSubmitting}
-                >
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
-          )}
-        />
-      </Card>
-    );
-  }
-}
+})(LoginForm); // currying functions in Javascript
 
 export default Login;
